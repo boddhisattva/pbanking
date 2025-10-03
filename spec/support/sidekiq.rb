259f9073c2711eaa1a2ace0da57a016554cb324a
Mohnish G J <mail4mohnish@gmail.com>
2025-10-03 15:55:09 +0200

    Update Sidekiq setup config to be used with Rspec
    Using 'rspec-sidekiq' gem gives us a lot of functionality out of the box, hence commenting all unnecessary code with regards to sidekiq setup

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,24 +1,33 @@
 
 # frozen_string_literal: true
 
-require "sidekiq/testing"
+# Allows using additional matchers for Sidekiq jobs in RSpec tests
+# rspec-sidekiq requires sidekiq/testing by default
+# Requiring sidekiq/testing will automatically call Sidekiq::Testing.fake!
 require "rspec-sidekiq"
 
 # Configure Sidekiq for test environment
 RSpec.configure do |config|
   # Set fake mode globally for ALL tests
-  # This ensures jobs are queued but never executed
-  config.before(:suite) do
-    Sidekiq::Testing.fake!
-  end
+  # This ensures Sidekiq jobs are queued but never executed
+  # The fake option is the default option for sidekiq/testing
+  # so we don't need to set it explicitly below
+  # config.before(:suite) do
+  #   Sidekiq::Testing.fake!
+  # end
 
-  # Clear all job queues before each test
-  # This prevents job contamination between tests
-  config.before do
-    Sidekiq::Worker.clear_all
-  end
+  # Worker queues are global and will therefore persist between tests.
+  # Clear all Sidekiq job queues before each test
+  # This prevents job and related state contamination between tests
+  # This also helps keep to make sure tests are order independent
+  # We don't need to clear the queues explicitly below if we are using rspec-sidekiq
+  # as rspec-sidekiq will clear the enqueued jobs automatically
+  # as mentioned here: https://github.com/wspurgin/rspec-sidekiq
+  # config.before do
+  #   Sidekiq::Worker.clear_all
+  # end
 
-  # Run jobs inline for tests marked with :sidekiq_inline metadata
+  # Run Sidekiq jobs inline for tests marked with :sidekiq_inline metadata
   config.around do |example|
     if example.metadata[:sidekiq_inline] == true
       Sidekiq::Testing.inline! { example.run }
@@ -26,7 +35,4 @@ RSpec.configure do |config|
       example.run
     end
   end
-
-  # Include rspec-sidekiq matchers for better assertions
-  config.include RSpec::Sidekiq::Matchers
 end
